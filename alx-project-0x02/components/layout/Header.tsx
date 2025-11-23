b5146224133f2c521a2c81f6bbec3e333fa6d728
juskins <omojuwababatunde1@gmail.com>
2025-11-23 06:30:11 +0100

    Add Link keyword

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 const Header = () => {
    return (
       <header className="w-full p-4 bg-gray-200 dark:bg-gray-800">
          <h1 className="text-2xl font-bold text-center">Welcome to My Website</h1>
          <nav>
             <ul>
-               <li><a href="/home">Home</a></li>
-               <li><a href="/about">About</a></li>
+               <li><Link href="/home">Home</Link></li>
+               <li><Link href="/about">About</Link></li>
             </ul>
          </nav>
       </header>
